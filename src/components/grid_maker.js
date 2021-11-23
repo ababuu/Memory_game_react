7eaae1237adbc3a771c66c3c9332c696503d0096
ababu <ababualemu123@gmail.com>
2021-11-23 20:59:22 +0300

    order assigned

--- src/components/grid_maker.js
+++ src/components/grid_maker.js
@@ -2,10 +2,19 @@ import React from "react";
 import './grid_maker.css'
 import pictures from './pictures.json'
 function MakeGrid(props){
+    const numbers=[1,2,3,4,5,6,7,8,9];
+    const handleClick=()=>{
+        alert('clikced')
+    }
+    pictures.forEach((pic,index)=>{
+        pic.id=numbers[index]
+    })
         return(
             <div className='grid_container'>
-                {pictures.map(p=>console.log(p.src))}
-                {pictures.map((picture)=><div className='gird_item'><img alt='' className='grid_image' src={picture.src}/><p className='name'>{picture.name}</p></div>)}
+                {pictures.map((picture, index)=>{
+                    
+                    return <div id={index+1} onClick={handleClick}><img alt='' className='grid_image' src={picture.src}/><p className='name'>{picture.name}</p></div>})}
+                    {console.log(pictures.map(p=>p.id))}
             </div>
         )
 }
